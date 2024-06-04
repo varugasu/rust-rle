3d588f41e33633f5afa6fbe92d4350880f059267
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2024-06-03 22:24:44 -0300

    feat: lre encode

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
 pub fn lre_encode(data: String) -> String {
-    data
+    let mut encoded_data = String::new();
+
+    if data.is_empty() {
+        return encoded_data;
+    }
+
+    let mut count = 0;
+    let mut current_char = data.chars().next().unwrap();
+    for c in data.chars() {
+        if c == current_char {
+            count += 1;
+        } else {
+            encoded_data.push(current_char);
+            encoded_data.push_str(&count.to_string());
+            current_char = c;
+            count = 1;
+        }
+    }
+
+    encoded_data.push(current_char);
+    encoded_data.push_str(&count.to_string());
+
+    encoded_data
 }
 
 pub fn lre_decode(data: String) -> String {
